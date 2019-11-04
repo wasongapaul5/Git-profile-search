2b8f37cc5d6b6bde64a7789a1560834d7bdf07c8
wasongapaul5 <wasongapaul5@gmail.com>
2019-11-04 17:38:02 +0300

    add files

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileService }from './profile-service/profile.service';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from 'selenium-webdriver/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProfileComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [ProfileService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/profile-service/profile.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders }from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+private username:string;
+private clientid = "d4db20ae57a9e8f462da";
+private clientsecret = "b3579b59810e8febbc0248bfa93303730e88b822";
+
+  constructor(private http:HttpClient) { 
+    console.log("Service is now ready!");
+    this.username = "wasongapaul5";
+  }
+  getProfileInfo(){
+    return this.http.get("https://api.github.com/users/" + this.username + "?client_id="+ this.clientid + "&client_secret=" + this.clientsecret)
+  }
+
+}
