5b1b7801ddb9d388b1bee6c72e9b593777a8e98c
wasongapaul5 <wasongapaul5@gmail.com>
2019-11-05 15:33:23 +0300

    import FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ProfileService }from './profile-service/profile.service';
 import { HttpClientModule } from '@angular/common/http';
-import {FormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
