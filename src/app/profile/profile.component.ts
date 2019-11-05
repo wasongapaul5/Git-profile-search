209a2174ce9f0c522f7e9d88a80a5d8b05214bfd
wasongapaul5 <wasongapaul5@gmail.com>
2019-11-05 12:06:28 +0300

    add styling to different files

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,34 +1,43 @@
-// import { Component, OnInit, } from '@angular/core';
-// import { ProfileService }from '../profile-service/profile.service';
+import { Component, OnInit, } from '@angular/core';
+import { ProfileService }from '../profile-service/profile.service';
 
-// @Component({
-//   selector: 'app-profile',
-//   templateUrl: './profile.component.html',
-//   styleUrls: ['./profile.component.css']
-// })
-// export class ProfileComponent implements OnInit {
-//   profile:any;
-//   repos:any;
-//   username:string;
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+  profile:any;
+  repos:any;
+  username:string;
 
-// constructor(private profileService:ProfileService){
+constructor(private profileService:ProfileService){
+  this.profileService.updateProfile(this.username);
+  this.profileService.getProfileInfo().subscribe(profile => {
+    console.log(profile);
+    this.profile = profile;
+  });
 
-// }
+  this.profileService.getProfileRepos().subscribe(repos => {
+    console.log(repos);
+    this.repos = repos;
+  });
+}
  
-// findProfile(){
-//   this.profileService.updateProfile(this.username);
-//   this.profileService.getProfileInfo().subscribe(profile => {
-//     console.log(profile);
-//     this.profile = profile;
-//   });
+findProfile(){
+  this.profileService.updateProfile(this.username);
+  this.profileService.getProfileInfo().subscribe(profile => {
+    console.log(profile);
+    this.profile = profile;
+  });
 
-//   this.profileService.getProfileRepos().subscribe(repos => {
-//     console.log(repos);
-//     this.repos = repos;
-//   })
-// }
+  this.profileService.getProfileRepos().subscribe(repos => {
+    console.log(repos);
+    this.repos = repos;
+  })
+}
 
-//   ngOnInit() {
-//   }
+  ngOnInit() {
+  }
 
-// }
+}
